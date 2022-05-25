f068e29d74e5442c7619269c2ffecf2d23227924
UO277084 <uo277084@uniovi.es>
2022-05-26 01:25:36 +0200

    Procesamiento de XML acabado
    Eso creo al menos

--- js/xmlManager.js
+++ js/xmlManager.js
@@ -26,17 +26,18 @@ class XMLManager {
                 var xml = $(reader.result);
 
                 //Obtenemos los datos del equipo
-                var teamData = $(xml).find("datosEquipo");
+                var teamData = $(xml).find("datosEquipo")[0];
                 var teamName = $(teamData).find("nombre").text();
                 var antiguedad = $(teamData).find("antiguedad").text();
                 var city = $(teamData).find("ciudad").text();
-                datos += "<h2>" + teamName + "</h2>";
-                datos += "<h3>Datos del equipo</h3>";
+                datos += "<h1>" + teamName + "</h1>";
+                datos += "<h2>Datos del equipo</h2>";
                 datos += "<ul>";
                 datos += "<li>Antiguedad: " + antiguedad + "</li>";
                 datos += "<li>Ciudad: " + city + "</li>";
                 datos += "</ul>";
 
+                datos += "<h2>Nacionalidades de jugadores</h2>";
                 //Cogemos los países
                 var countries = $(xml).find("pais")
                 countries.each(function () {
@@ -61,23 +62,60 @@ class XMLManager {
                     //Cogemos los jugadores del país
                     var jugadores = $(this).find("jugador");
                     jugadores.each(function () {
+                        datos += "<figure>";
                         //Cogemos los datos del jugador
                         var nombre = $(this).attr("nombre").valueOf() + " " + $(this).attr("apellidos").valueOf();
-                        var fechaNac = $(this).find("fechaNac");
+                        var fechaNac = $(this).find("datosJugador").find("fechaNac");
                         var fechaDia = fechaNac.attr("dia").valueOf();
                         var fechaMes = fechaNac.attr("mes").valueOf();
                         var fechaYear = fechaNac.attr("year").valueOf();
-                        var altura = $(this).find("altura").text();
-                        var ciudad = $(this).find("ciudad").text();
+                        var altura = $(this).find("datosJugador").find("altura").text();
+                        var ciudad = $(this).find("datosJugador").find("ciudad").text();
 
                         //Indicamos como se mostrarán los datos del jugador
-                        datos += "<h5>Datos de " + nombre + "</h5>";
+                        datos += "<h5>" + nombre + "</h5>";
                         datos += "<ul>";
                         datos += "<li>Fecha de nacimiento: " + fechaDia + "/" + fechaMes + "/" + fechaYear + "</li>";
                         datos += "<li>Altura: " + altura + "</li>";
                         datos += "<li>Ciudad: " + ciudad + "</li>";
+                        //Poner una href para ver la foto (?)
                         datos += "</ul>";
-                        //Mostrar los datos de los equipos antiguos
+
+                        datos += "<figure>";
+                        //Cogemos los equipos antiguos
+                        var equiposAntiguos = $(this).find("equipo");
+                        if (equiposAntiguos.length > 1) {
+                            datos += "<h6>Sus últimos equipos han sido:</h6>";
+                        } else if (equiposAntiguos.length == 1) {
+                            datos += "<h6>Su último equipo ha sido:</h6>";
+                        } else {
+                            datos += "<h6>Solo ha jugado en este equipo</h6>";
+                        }
+
+                        equiposAntiguos.each(function () {
+
+                            var oldTeamData = $(this).find("datosEquipo");
+                            var oldTeamName = $(oldTeamData).find("nombre").text();
+                            var oldTeamFechaIni = $(oldTeamData).find("fechaIni");
+                            var oldTeamFechaFin = $(oldTeamData).find("fechaFin");
+                            var oldTeamAntiguedad = $(oldTeamData).find("antiguedad").text();
+                            var oldTeamCity = $(oldTeamData).find("ciudad").text();
+
+                            //Indicamos como se mostrarán los datos del equipo antiguo
+                            datos += "<h4>" + oldTeamName + "</h4>";
+                            datos += "<ul>";
+                            datos += "<li>Fecha inicio contrato: " + oldTeamFechaIni.attr("dia").valueOf() +
+                                "/" + oldTeamFechaIni.attr("mes").valueOf() + "/" +
+                                oldTeamFechaIni.attr("year").valueOf() + "</li>";
+                            datos += "<li>Fecha fin contrato: " + oldTeamFechaFin.attr("dia").valueOf() +
+                                "/" + oldTeamFechaFin.attr("mes").valueOf() + "/" +
+                                oldTeamFechaFin.attr("year").valueOf() + "</li>";
+                            datos += "<li>Antiguedad: " + oldTeamAntiguedad + "</li>";
+                            datos += "<li>Ciudad: " + oldTeamCity + "</li>";
+                            datos += "</ul>";
+                        });
+                        datos += "</figure>";
+                        datos += "</figure>";
                     });
                 });
                 //Mostramos los datos del equipo
